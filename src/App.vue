bda2e50df184fd8fc992d081fd61dc6efab43df0
almina38 <alminabor@live.nl>
2025-07-05 13:39:13 +0200

    fix bug: de kaarten kunnen niet meer aangeklikt worden terwijl 2 open kaarten wachten op terugdraaien

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,36 @@
 <template>
   <h1 class="sr-only">Vue-niverse</h1>
-<img src="/images/title.png" 
-  alt="Vue-niverse" class="title"
-  :style="{ width: '550px', height: 'auto' }" />
-  <section class="description"><p>
-    Welcome to Vue-niverse,
-  </p><p>
-    A card matching game powered by Vue.js 3!
-  </p>
-</section>
-  <transition-group tag="section" 
-  class="game-board" name="shuffle-card">
-    <Card
-    v-for="card, in cardList"
-    :key="`${card.value}-${card.variant}`"
-    :matched="card.matched"
-    :value="card.value" 
-    :visible="card.visible"
-    :position="card.position"
-    @select-card="flipCard"/>
-    
-  </transition-group>
-    <h2 class="status">{{ status }}</h2>
-  <button v-if="newPlayer" @click="startGame"
-  class="button">
-   <img src="../public/images/play.svg" alt="Restart Icoon">
-  Start game</button>
+  <img src="/images/title.png" 
+    alt="Vue-niverse" class="title"
+    :style="{ width: '550px', height: 'auto' }" />
+  <section class="description">
+    <p>Welcome to Vue-niverse,</p>
+    <p>A card matching game powered by Vue.js 3!</p>
+  </section>
 
-  <button v-else @click="restartGame"
-  class="button">
-   <img src="../public/images/restart.svg" alt="Restart Icoon">
-  Restart game</button>
+  <transition-group tag="section" 
+    class="game-board" name="shuffle-card">
+    <Card
+      v-for="card in cardList"
+      :key="`${card.value}-${card.variant}`"
+      :matched="card.matched"
+      :value="card.value" 
+      :visible="card.visible"
+      :position="card.position"
+      @select-card="flipCard"/>
+  </transition-group>
+
+  <h2 class="status">{{ status }}</h2>
+
+  <button v-if="newPlayer" @click="startGame" class="button">
+    <img src="../public/images/play.svg" alt="Restart Icoon">
+    Start game
+  </button>
+
+  <button v-else @click="restartGame" class="button">
+    <img src="../public/images/restart.svg" alt="Restart Icoon">
+    Restart game
+  </button>
 </template>
 
 <script>
@@ -41,56 +41,54 @@ import Card from './components/Card.vue'
 
 export default {
   name: 'App',
-  components:{
+  components: {
     Card
   },
-  setup(){
+  setup() {
     const cardList = ref([])
     const userSelection = ref([])
     const newPlayer = ref(true)
+    const lockBoard = ref(false)
 
     const startGame = () => {
       newPlayer.value = false
-
       restartGame()
     }
 
     const status = computed(() => {
-      if (remainingPairs.value === 0){
+      if (remainingPairs.value === 0) {
         return 'Player wins!'
       } else {
         return `Remaining Pairs: ${remainingPairs.value}`
       }
     })
 
-
     const remainingPairs = computed(() => {
       const remainingCards = cardList.value.filter(
         card => card.matched === false
       ).length
-
       return remainingCards / 2
     })
 
- 
     const restartGame = () => {
       cardList.value = _.shuffle(cardList.value)
 
-      cardList.value = cardList.value.map((card, 
-      index) => {
-        return{
+      cardList.value = cardList.value.map((card, index) => {
+        return {
           ...card,
           matched: false,
           position: index,
           visible: false
         }
       })
+
+      userSelection.value = []
     }
 
     const cardItems = ['alien', 'asteroid', 'astronaut', 'blackhole', 'comet', 
-    'rocket', 'saturn', 'sun']
+      'rocket', 'saturn', 'sun']
 
-    cardItems.forEach(item =>{
+    cardItems.forEach(item => {
       cardList.value.push({
         value: item,
         variant: 1,
@@ -107,67 +105,63 @@ export default {
       })
     })
 
-    cardList.value = cardList.value.map((card, 
-    index) => {
-      return{
+    cardList.value = cardList.value.map((card, index) => {
+      return {
         ...card,
         position: index
       }
     })
 
- 
     const flipCard = (payload) => {
+      if (lockBoard.value) return
       cardList.value[payload.position].visible = true
 
-
-      if (userSelection.value[0]){
-        if(userSelection.value[0].position ===
-          payload.position && 
-          userSelection.value[0].faceValue 
-          === payload.faceValue
-        ){
+      if (userSelection.value[0]) {
+        if (
+          userSelection.value[0].position === payload.position &&
+          userSelection.value[0].faceValue === payload.faceValue
+        ) {
           return
-        } else{
-         userSelection.value[1] = payload
-
+        } else {
+          userSelection.value[1] = payload
         }
       } else {
         userSelection.value[0] = payload
       }
     }
-    watch(remainingPairs, currentValue=>{
-      if (currentValue === 0){
+
+    watch(remainingPairs, currentValue => {
+      if (currentValue === 0) {
         launchConfetti()
       }
     })
-    watch(userSelection,currentValue => {
-      if (currentValue.length === 2){
+
+    watch(userSelection, currentValue => {
+      if (currentValue.length === 2) {
         const cardOne = currentValue[0]
         const cardTwo = currentValue[1]
 
-        if(cardOne.faceValue == cardTwo.faceValue){
-        
-          cardList.value[cardOne.position].
-          matched = true
-          cardList.value[cardTwo.position].
-          matched = true
-        }else{
+        lockBoard.value = true
+
+        if (cardOne.faceValue == cardTwo.faceValue) {
+          cardList.value[cardOne.position].matched = true
+          cardList.value[cardTwo.position].matched = true
+          lockBoard.value = false
+        } else {
           setTimeout(() => {
-          cardList.value[cardOne.position]
-          .visible = false
-          cardList.value[cardTwo.position]
-          .visible = false
+            cardList.value[cardOne.position].visible = false
+            cardList.value[cardTwo.position].visible = false
+            lockBoard.value = false
           }, 1500)
         }
-        userSelection.value.length = 0
-        
-      } 
 
-    }, {  deep: true})
-    
-    return{
+        userSelection.value.length = 0
+      }
+    }, { deep: true })
+
+    return {
       cardList,
-      flipCard, 
+      flipCard,
       userSelection,
       status,
       restartGame,
@@ -177,6 +171,7 @@ export default {
   }
 }
 </script>
+
 <style>
 html, body {
   margin: 0;
@@ -287,7 +282,6 @@ h1 {
   transition: transform 0.8s ease-in;
 }
 
-/* Media Queries voor Responsiveness */
 @media (max-width: 768px) {
   .game-board {
     grid-template-columns: repeat(4, 90px);
@@ -295,7 +289,7 @@ h1 {
     grid-gap: 10px;
   }
 
-  .title{
+  .title {
     max-width: 450px;
   }
 
@@ -333,6 +327,4 @@ h1 {
     max-width: 300px;
   }
 }
-
-
 </style>
